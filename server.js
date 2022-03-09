b29bd9c629a285a034a834c5cde7c814c0eaefa1
Emmanuel Oteng Wilson <hagioswilson@gmail.com>
2022-03-09 14:22:42 +0000

    added category and categoryproduct type to the schema

--- server.js
+++ server.js
@@ -9,35 +9,61 @@ const typeDefs = gql`
         name: String,
         test: String,
         aray: [Int],
-        products: [Product!]!
+        products: [Product!]!,
+        product(id: ID!): Product,
+        categories: [Category],
+        category(id: ID): Category,
+        categoryProduct(id: ID): CategoryProduct
     }
 
     type Product {
+        id: String!,
         name: String!,
         description: String!,
         quantity: Int!,
         price: Float!,
+        image: String!
         onSale: Boolean!
+    }
+
+    type Category {
+        id: String!,
+        name: String!
+    }
+
+    type CategoryProduct {
+        id: String!,
+        name: String!,
+        description: String!,
+        quantity: Int!,
+        price: Float!,
+        image: String!
+        onSale: Boolean!
+        categoryId: String!
     }
 `
 
 const resolvers = {
     Query: {
-        name : () => {
-            return 'Emmanuel'
-        },
-
-        test: () => {
-            return 'Test of time'
-        },
-
-        aray: () => {
-            return [12, 5, 2, 67]
-        },
-
         products: () => {
             return products
-        }
+        },
+
+        product: (parent, args, context ) => {
+            return products.find(p => p.id === args.id)
+        },
+
+        categories: () => {
+            return categories
+        },
+
+        category: (parent, args, context ) => {
+            return categories.find(c => c.id === args.id)
+        },
+
+        categoryProduct: (parent, args, context ) => {
+            return categoryProduct.find(p => p.categoryId === args.id)
+        },
     }
 }
 
